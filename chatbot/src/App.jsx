3940030015937e0c50b589b056f1a36676c59ec3
anandch17 <anandch119@gmail.com>
2024-06-22 22:20:11 +0530

    chatbot

---
+++ chatbot/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Chat from './Component/Chat.jsx'
+
+
+function App() {
+ 
+
+  return (
+    <Chat/>
+  )
+}
+
+export default App
